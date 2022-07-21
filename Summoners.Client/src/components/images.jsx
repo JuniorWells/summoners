ce6d0a01dd9f68ee9a8de1f1cc0aa66dbf4cd8d4
syed-yadish <yadish@live.com>
2022-07-21 21:43:09 +0200

    image border

--- Summoners.Client/src/components/images.jsx
+++ Summoners.Client/src/components/images.jsx
@@ -10,11 +10,21 @@ import jhin from '../assets/images/Jhin.png'
 import nilah from '../assets/images/Nilah.png'
 import yasuo from '../assets/images/Yasuo.png'
 import yone from '../assets/images/Yone.png'
+import new1 from '../assets/images/test.png'
+import new2 from '../assets/images/test2.png'
+import new3 from '../assets/images/test3.png'
+import new4 from '../assets/images/test4.png'
+import new5 from '../assets/images/test5.png'
 
 export const SliderData = [
     { image: akali }, { image: lux }, { image: Ashe }, { image: Garen }, 
     { image: jhin },
     { image: Ahri }, 
+    { image: new1 },
+    { image: new2 },
+    { image: new3 },
+    { image: new4 },
+    { image: new5 },
     { image: ornn }, 
     { image: nilah }, 
     { image: bel },  
