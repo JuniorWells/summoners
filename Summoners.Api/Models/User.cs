e8ded829ac738d0c7ff93fda6dce58108b6bad1b
syed-yadish <yadish@live.com>
2022-07-25 11:09:24 +0200

    obj deleted3

--- Summoners.Api/Models/User.cs
+++
@@ -1,8 +0,0 @@
-namespace Summoners.Api.Models;
-
-public class User
-{
-    public int Id { get; set; }
-    public string? SummonerName { get; set; }
-    public List<Post>? Posts { get; set; }
-}
