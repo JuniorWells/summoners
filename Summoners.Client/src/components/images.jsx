5b37d7d2082a4426b3bbf1aa90566d4348b1c08a
JuniorWells <kostas.chaniotakis@gmail.com>
2022-07-26 11:53:12 +0200

    remove duplicates

--- Summoners.Client/src/components/images.jsx
+++ Summoners.Client/src/components/images.jsx
@@ -2,32 +2,27 @@ import logo from '../assets/images/logo.png';
 import Ashe from '../assets/images/Champion_ashe_HP.png'
 import Ahri from '../assets/images/Champion_Ahri_HP.png'
 import Garen from '../assets/images/Champion_garen_HP.png'
-import akali from '../assets/images/akali_card.png'
 import lux from '../assets/images/lux_card.png'
 import ornn from '../assets/images/Ornn.png'
-import bel from '../assets/images/Bel.png'
 import jhin from '../assets/images/Jhin.png'
 import nilah from '../assets/images/Nilah.png'
 import yasuo from '../assets/images/Yasuo.png'
 import yone from '../assets/images/Yone.png'
-import new1 from '../assets/images/test.png'
 import new2 from '../assets/images/test2.png'
 import new3 from '../assets/images/test3.png'
 import new4 from '../assets/images/test4.png'
 import new5 from '../assets/images/test5.png'
 
 export const SliderData = [
-    { image: akali }, { image: lux }, { image: Ashe }, { image: Garen }, 
+    { image: lux }, { image: Ashe }, { image: Garen }, 
     { image: jhin },
     { image: Ahri }, 
-    { image: new1 },
     { image: new2 },
     { image: new3 },
     { image: new4 },
     { image: new5 },
     { image: ornn }, 
-    { image: nilah }, 
-    { image: bel },  
+    { image: nilah },
     { image: yasuo }, 
     { image: yone }
   ];
